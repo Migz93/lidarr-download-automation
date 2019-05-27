20b936646b6ebff1c094c57965458cf71b947824
Migz93 <33037112+Migz93@users.noreply.github.com>
2019-05-27 17:19:41 +0100

    Legacy & New
    Move old scripts to legacy folder, update readme to explain about new version.

--- README.md
+++ README.md
@@ -1,67 +1,53 @@
-# modified usage
-Edit the "config.sample" file  with your variables and save as config
-run lidarr-smloadr2.sh
-
-# what changed
-Added a config file.<br>
-Added a way to grab albums marked as wanted in lidarr instead of everything by an artist. Change mode=wanted in config to use it, change mode=artist to use the old method.<br>
-Added a connection to Lidarr to allow automatic importing of downloads.<br>
-Removed dependency on python.<br>
-
-The powershell version remains unchanged
-
 # lidarr-smloadr-script 
-Bash & powershell scripts to download artists in lidarr using smloadr.
+Bash & powershell scripts to download artists in Lidarr using smloadr.
 
-I created this to fill a need i had, i have lidarr installed but it doesn't grab many releases which is more a fault of my lack of indexers but still. <br>
+I created the original version of this to fill a need i had, i have Lidarr installed but it doesn't grab many releases which is more a fault of my lack of indexers but still. <br>
 I then found out about smloadr and tried to download every single artist, this left me with a lot of junk that i'd never listen to.<br>
 I then tried just downloading genres i like but again left me with a lot of junk i'd never listen to.<br>
 
-So i set about trying to find something that would bridge the gap between the artists i have monitored in lidarr & downloading them with smloadr, I couldn't find anything at all for this so i set about creating my own script to do this.
-<br> I'm by no means anywhere close to a scripter so there probably will be better ways of scripting then i've done but it works for me so I wanted to share incase anyone else wanted to use it.
+So i set about trying to find something that would bridge the gap between the artists i have monitored in Lidarr & downloading them with smloadr, I couldn't find anything at all for this so i set about creating my own script to do this.
+See scripts within [Legacy](Legacy/LEGACY.md) folder.
 
-It will pull all your artists set up in lidarr and then go through these one by one, lidarr sometimes has the deezer ID stored in the data that it collects from Musicbrainz, if it has this then it will save this ID to a file to be used by smloadr, if it doesn't have the ID then it will search the Deezer API using artist name & the artists last album, taking the first hit and saving this to a txt file which can then be used in smloadr.<br>
-Sometimes last album is not available from Lidarr so it will fall back to searching with just the artist name.<br>
+[permutationalparody](https://github.com/permutationalparody) then improved the script in various ways, including adding a seperate config file, adding an option to only search for albums/EPs/singles/ect that are set to monitored in Lidarr, add an option to allow Lidarr to import the downloaded files and generally tidied up the code a large ammount.
 
-If the Deezer ID is provided by Lidarr then this should be 100% accurate, lidarr collects its information from https://musicbrainz.org which is open to anyone to edit, so if the ID is incorrect or missing you can sign up for an account and ammend/add this yourself.
-If the ID isn't provided and it does need to do a manual search then even though this is a very very simple search of just searching with artist name + last album or just artist name, it's still fairly accurate and out of 169 artists it's correctly matched 161 of them. Although bear in mind if it does match incorrectly it can sometimes be a large mistake I.e having "Dua Lipa" in my Lidarr initially matched to https://www.deezer.com/us/artist/1198498 which ends up as a 100GB+ download.<br>
-You can manually add in your own deezer artist IDs into the generated "lidarr-smloadr.txt" file to fix any incorrect grabs, theres no way yet to remove the incorrect grabs though so it will also download those.<br>
-You can review the log file generated in the $scriptdir, this log shows the deezer url returned for the artist, along with the specific search query that was used to find this or if the ID was grabbed from Lidarr/Musicbrainz directly.<br>
+# Modes
+This script has two modes (configured by changing the "mode" paramater in the config file):
 
-Lidarr should pick up the files and show them as downloaded if you have it pointed at the same place your files download.
+<b>Wanted:</b>
+This it the prefered mode to use, with this option the script will check all artists & check their last album and attempt to find the Deezer ID from this data. If it fails and the "EnableFuzzyAlbumSearch" paramater is "True" then it will fall back to carrying out a fuzzy search of Deezer using the Artist Name & Last Album Name. If it's still unable to find a Deezer ID then the artist will be saved to the skiplog file configured in config. 
 
-I'm not a script wizard in the slightest, not in bash or powershell. So if you see anything that could be improved in either then please let me know.
+Once a Deezer ID has been identified it will then pass this through to smloadr to download, downloading to the path & at the quality speecified within config.
 
+If "EnableLidarrProcess" is set to "True" then once the download is complete it will inform Lidarr of the download location, allowing Lidarr to match & import these files.
+This mode will only download albums/EPs/singles/ect that you have actually set to monitored within Lidarr.
 
-# lidarr-smloadr-script - Bash Version
+<b>Artist:</b>
+This is the way the original legacy script would use, with this option the script will check all artists & check their last album and attempt to find the Deezer ID from this dat. If it fails then it will fall back to carrying out a fuzzy search of Deezer using the Artist Name & Last Album Name. If it's still unable to find a Deezer ID then the artist will be saved to the skiplog file configured in config.
+This mode will download everything by the artist that Deezer provides, so you can end up with a lot of unwanted albums/EPs/singles/ect.
 
-Requirements:
-* smloadr downloaded & ideally in the same location as the script. I personally bundle them both in /opt/smloadr <br>
-* jq installed (sudo apt-get install jq)
-* python version 2.7 installed (sudo apt-get install python)
+# Config
+Edit the "config.sample" file, fill your paramaters and save as "config".
+* scriptDir - 				Directory that you want log file & artist ID batch file to be stored.<br>
+* downloadDir - 			Directory that you want smloadr to download to.<br>
+* LidarrUrl - 				Set domain or IP to your Lidarr instance including port. If using reverse proxy, do not use a trailing slash.<br>
+* LidarrApikey - 			Lidarr api key.<br>
+* quality - 				SMLoadr Download Quality setting (MP3_128,MP3_320,FLAC).<br>
+* logname -					Log file name.<br>
+* skiplogname -				Logs any info if an item was skipped.<br>
+* mode -					Mode to choose what to scrape from Lidarr, wanted gets only the albums that are marked wanted, artist gets all the albums from the monitored artists.<br>
 
-Varriables. Edit lines 1-14 to fill in your variables: <br>
-* scriptDir - #Directory that you want log file & artist ID batch file to be stored.<br>
-* downloadDir - Directory that you want smloadr to download to.<br>
-* lidarrUrl - iSet domain or IP to your lidarr instance including port. If using reverse proxy, do not use a trailing slash.<br>
-* lidarrApikey - Lidarr api key.<br>
-* fallbackSearch - Fallback to searching if lidarr doesn't provide deezer ID, only supports "true", if anything else it won't fallback.
-* quality - SMLoadr Download Quality setting (MP3_128,MP3_320,FLAC)
+Below are only used if "mode" is set to "wanted".
+* wantedalbumsamount -		The amount of wanted albums to process it will grab the newest x amount of albums from the Lidarr wanted list.<br>
+* EnableLidarrProcess -		Set to True to instruct Lidarr to process the download once smloadr finishes.<br>
+* EnableFuzzyAlbumSearch -	Set to True to enable fuzzy album search if theres no exact match.<br>
 
-Tested and working on Ubuntu 16.04 but should work on later versions
+# Requirements
+* Lidarr installed & running.<br>
+* smloadr downloaded & ideally in the same location as the script. I personally bundle them both in /opt/smloadr.<br>
+* jq installed (sudo apt-get install jq).<br>
 
-# lidarr-smloadr-script - Powershell Version
-(Powershell version currently does not check if Lidarr has provided the Deezer ID. This version only takes your artist + last album and searches Deezer for it.
+# Other
+Lidarr collects its information from https://musicbrainz.org which is open to anyone to edit, so if the Deezer ID in Lidarr is incorrect or missing you can sign up for an account and ammend/add this yourself.
 
-Requirements:
-* smloadr downloaded & ideally added to your path, if not you would need to edit the smloadr download line towards the end to point at the location it's in. <br>
-* rclone downloaded & idealy added to your path,  if not you would need to edit the rclone copy line towards the end to point at the location it's in. Only required if you're planning on using rclone, if not you can just remove/comment out this line.<br>
-
-Varriables. Edit lines 1-10 to fill in your variables: <br>
-* $scriptDir - #Directory that you want log file & artist ID batch file to be stored.<br>
-* $downloadDir - Directory that you want smloadr to download to.<br>
-* $lidarrurl is either the domain or IP of the machine running lidarr.<br>
-* $lidarrport is the port that lidarr runs on.<br>
-* $lidarrApikey - Lidarr api key.<br>
-
-Tested and working on Windows 10.
+Original Script: Myself
+Improved Script:[permutationalparody](https://github.com/permutationalparody)
--- lidarr-smloadr.sh
+++ lidarr-smloadr.sh
@@ -1,112 +1,219 @@
 #!/bin/bash
 
-#Directory that you want log file & artist ID batch file to be stored.
-scriptDir="/opt/smloadr/lidarr"
-#Directory that you want smloadr to download to.
-downloadDir="/mnt/unionfs/Media/Music/"
-#Set domain or IP to your lidarr instance including port. If using reverse proxy, do not use a trailing slash. Ensure you specify http/s.
-lidarrUrl="192.168.1.x"
-#Lidarr api key
-lidarrApiKey="08d108d108d108d108d108d108d108d1"
-#Fallback to searching if lidarr doesn't provide deezer ID, only supports "true", if anything else it won't fallback.
-fallbackSearch="true"
-#SMLoadr Download Quality setting (MP3_128,MP3_320,FLAC)
-quality="MP3_320"
-
-#Test if script dir doesn't exist, if true then create directory.
-if [ ! -d "$scriptDir" ]; then
-  mkdir -p $scriptDir
-fi
-
-echo "Collecting data from lidarr, this may take some time depending on how many artists you have." 
-curl "$lidarrUrl/api/v1/Artist/?apikey=$lidarrApiKey" -o $scriptDir/artists-lidarr.json
-artists=$(cat $scriptDir/artists-lidarr.json | jq -r '.[].sortName')
-
-totalArtists=$(wc -l <<< "$artists")
-totalArtists=$((totalArtists-1))
-echo $totalArtists total artists pulled from Lidarr
-for ((i=1;i<=totalArtists;i++));
-do
-	#create wantedArtist variable from sortName provided by lidarr
-	wantedArtist=$(cat $scriptDir/artists-lidarr.json | jq -r ".[$i].sortName")
-	#create lastAlbum variable from lastAlbum provided by lidarr.
-	lastAlbum=$(cat $scriptDir/artists-lidarr.json | jq -r ".[$i].lastAlbum | .title")
-	#Attempt to pull deezer link directly from lidarr if it is provided by musicbrainz.
-	wantedArtistID=$(cat $scriptDir/artists-lidarr.json | jq -r ".[$i].links | .[]")
-	wantedArtistID=$(echo $wantedArtistID | jq 'select(.name=="deezer") | .url')
-	
-	#If deezer link not provided by lidarr then fall back to search method if enabled.
-	if [ "$wantedArtistID" = "" ] && [ "$fallbackSearch" = "true" ];
-	then
-	lidarrGrab="False"
-	#Check if lastAlbum variable doesn't exist. Sometimes this isn't provided by lidarr.
-		if [ "$lastAlbum" = "null" ];
-		then
-			#Search deezer using only artist name. Not as accurate as using with last album name but better then nothing.
-			searchQuery="$wantedArtist"
+ArtistsLidarrReq(){
+	wantit=$(curl -s --header "X-Api-Key:"${lidarrApiKey} --request GET  "$lidarrUrl/api/v1/Artist/")
+}
+GetTotalArtistsLidarrReq(){
+	TotalLidArtistNames=$(echo "${wantit}"|jq -r '.[].sortName' |wc -l  )
+}
+ProcessArtistsLidarrReq(){
+	LidArtistName=$(echo "${wantit}" | jq -r .[$i].sortName)
+	LidAlbumName=$(echo "${wantit}" | jq -r ".[$i].lastAlbum.title")
+	#M1 -- retrieve deezer artist id -- from lidarr
+	DeezerArtistURL=$(echo "${wantit}" | jq ".[$i].links[] "|jq -r 'select(.name=="deezer")|.url')
+	DeezerArtistID=$(printf -- "%s" "${DeezerArtistURL##*/}")
+	if [ "$LidAlbumName" = "null" ]; then
+		if [ "${DeezerArtistURL}" = "" ] || [ "${DeezerArtistID}" = "" ]; then
+			##M2 fallback -- retrieve deezer artist id -- from deezer
 			#Encode searchQuery in a url encodable format.
-			searchQuery=$(/usr/bin/python -c "import urllib, sys; print urllib.quote(sys.argv[1])"  "$searchQuery")
-			wantedArtistID=$(curl -s https://api.deezer.com/search?q=$searchQuery | jq -r ".data | .[0] | .artist | .id")	
-		else
-			#Otherwise if lastAlbum variable exists. Generate searchQuery variable, take first result and set the artistID from deezer as variable wantedArtistID.
-			searchQuery="$wantedArtist%20$lastAlbum"
-			#Encode searchQuery in a url encodable format.
-			searchQuery=$(/usr/bin/python -c "import urllib, sys; print urllib.quote(sys.argv[1])"  "$searchQuery")
-			wantedArtistID=$(curl -s https://api.deezer.com/search?q=$searchQuery | jq -r ".data | .[0] | .artist | .id")
-			#Check if wantedArtistID is empty following search, if so it means no results were found, fall back to searching with just artist.
-			if [ "$wantedArtistID" = "null" ];
-			then
-				searchQuery="$wantedArtist"
-				#Encode searchQuery in a url encodable format.
-				searchQuery=$(/usr/bin/python -c "import urllib, sys; print urllib.quote(sys.argv[1])"  "$searchQuery")
-				wantedArtistID=$(curl -s https://api.deezer.com/search?q=$searchQuery | jq -r ".data | .[0] | .artist | .id")
-			fi
+			searchQuery="${LidArtistName// /%20}"
+			searchQuery="https://api.deezer.com/search?q=${searchQuery}"
+			DeezerSearch=$(curl -s "${searchQuery}" | jq )
+			DeezerArtistID=$(echo "${DeezerSearch}" |jq -r ".data | .[]|.artist|.id" |uniq -c|sort -nr |head -n1 | awk '{print $2}')
+			DeezerArtistURL="https://www.deezer.com/artist/"${DeezerArtistID}
 		fi
-	else
-	lidarrGrab="True"
-	if [ "$wantedArtistID" = "" ] && [ "$fallbackSearch" = "false" ];
-	then
-		lidarrGrab=""
+	else 
+		if [ "${DeezerArtistURL}" = "" ] || [ "${DeezerArtistID}" = "" ]; then
+			##M3 fallback -- retrieve deezer artist id using last album-- from deezer
+			searchQuery="${LidArtistName// /%20}%20${LidAlbumName// /%20}"
+			searchQuery="https://api.deezer.com/search?q=${searchQuery}"
+			DeezerSearch=$(curl -s "${searchQuery}" | jq )
+			DeezerArtistID=$(echo "${DeezerSearch}" | jq -r ".data | .[]|.artist|.id" |uniq -c|sort -nr |head -n1 | awk '{print $2}')
+			DeezerArtistURL="https://www.deezer.com/artist/"${DeezerArtistID}
+		fi
 	fi
+##returns the wanted artists id -- from lidarr or deezer
+}
+
+AlbumsLidarrReq(){
+	wantit=$(curl -s --header "X-Api-Key:"${lidarrApiKey} --request GET  "$lidarrUrl/api/v1/wanted/missing/?page=1&pagesize=${wantedalbumsamount}&includeArtist=true&monitored=true&sortDir=desc&sortKey=releaseDate")
+}
+GetTotalAlbumsLidarrReq(){
+	TotalLidAlbumsNames=$(echo "${wantit}"|jq -r '.records[].title' |wc -l  )
+}
+
+ProcessAlbumsLidarrReq(){
+	LidArtistName=$(echo "${wantit}" | jq -r .records[${i}].artist.sortName)
+	LidArtistDLName=$(echo "${wantit}" | jq -r .records[${i}].artist.artistName)
+	LidAlbumName=$(echo "${wantit}" | jq -r .records[${i}].title)
+	#M1 -- retrieve deezer artist id -- from lidarr
+	DeezerArtistURL=$(echo "${wantit}" | jq -r .records[${i}].artist.links[] |jq -r 'select(.name=="deezer")|.url');
+	DeezerArtistID=$(printf -- "%s" "${DeezerArtistURL##*/}")
+	if [ "${DeezerArtistURL}" = "" ] || [ "${DeezerArtistID}" = "" ]; then
+		##M2 fallback -- retrieve deezer artist id -- from deezer
+		#Encode searchQuery in a url encodable format.
+		searchQuery="${LidArtistName// /%20}"
+		searchQuery="https://api.deezer.com/search?q=${searchQuery}"
+		DeezerArtistID=$(curl -s "${searchQuery}" | jq -r ".data | .[]|.artist|.id" |uniq -c|sort -nr |head -n1 | awk '{print $2}')
 	fi
+##returns the wanted artists id -- from lidarr or deezer
+}
 
-if [ "$lidarrGrab" = "True" ];
-then
-	#Save output to log file. Save wantedArtistID to wantedArtistID.txt file which will be used by smloadr.
-	echo "$wantedArtistID found for artist $i - $wantedArtist - Found using data from Lidarr." >> $scriptDir/lidarr-smloadr.log
-	echo "$wantedArtistID found for artist $i - $wantedArtist - Found using data from Lidarr."
-elif [ "$lidarrGrab" = "False" ];
-then
-	#Save output to log file. Save wantedArtistID to wantedArtistID.txt file which will be used by smloadr.
-	echo "https://www.deezer.com/artist/$wantedArtistID found for artist $i - $wantedArtist - Found using search query https://api.deezer.com/search?q=$searchQuery" >> $scriptDir/lidarr-smloadr.log
-	echo "https://www.deezer.com/artist/$wantedArtistID found for artist $i - $wantedArtist - Found using search query https://api.deezer.com/search?q=$searchQuery"
-elif [ "$lidarrGrab" = "" ];
-then
-	#Save output to log file. Save wantedArtistID to wantedArtistID.txt file which will be used by smloadr.
-	echo "Nothing found for artist $i - $wantedArtist. Possibly fallbackSearch is not enabled?" >> $scriptDir/lidarr-smloadr.log
-	echo "Nothing found for artist $i - $wantedArtist. Possibly fallbackSearch is not enabled?"
-fi
-	
-	#Save wantedArtistID to wantedArtistID.txt file which will be used by smloadr.
-	echo $wantedArtistID >> $scriptDir/wantedArtistID.txt
+QueryAlbumURL(){
+	##retrieve all albums for artist -- from deezer
+	searchQuery="https://api.deezer.com/artist/${DeezerArtistID}/albums&limit=1000"
+	DeezerDiscog=$(curl -s "${searchQuery}"| jq -r .);
+	DeezerDiscogTotal=$(echo "${DeezerDiscog}" |jq -r '.total')
+	mapfile -t DeezerDiscogArr <<< $(echo ${DeezerDiscog}|jq -c '.[][]?.title')
+	##match the wanted album title -- from deezer
+	for ((x=0;x<=DeezerDiscogTotal-1;x++)); do
+		DeezerDiscogAlbumName=$(echo "${DeezerDiscog}" |jq ".[]|.[$x]?"|jq -r .title )
+		if [ "${LidAlbumName,,}" = "${DeezerDiscogAlbumName,,}" ];then
+			DeezerAlbumURL=$(echo "${DeezerDiscog}" |jq ".[]|.[$x]?"|jq -r .link )
+			break
+		fi
+	done
+	if [ -z "${DeezerAlbumURL}" ] && [ "${EnableFuzzyAlbumSearch}" = True ];then
+		logit "Trying fuzzy search"
+		SanArtist="${LidArtistName// /%20}"
+		SanAlbum="${LidAlbumName// /%20}"
+		searchQuery="q=artist:\"${SanArtist//[^[:alnum:]%]}\"&q=album:\"${SanAlbum//[^[:alnum:]%]}\""
+		searchQuery="https://api.deezer.com/search?${searchQuery}"
+		DeezerDiscogFuzzy=$(curl -s "${searchQuery}");
+		DeezerAlbumID=$(echo "${DeezerDiscogFuzzy}" |jq '.[]|.[]?'|jq -r --argjson  DeezerArtistID "$DeezerArtistID" 'select(.artist.id==$DeezerArtistID) |.album.id'|sort -u|head -n1)
+		if [ -n "${DeezerAlbumID}" ];then
+			DeezerAlbumURL="https://www.deezer.com/album/${DeezerAlbumID}"
+			logit "Fuzzy search match ${DeezerAlbumURL}"
+		fi
+			logit "Fuzzy search cant find a match"
+	fi
+##returns wanted album URL -- from deezer
+}
 
-	#Small sleep to not hammer deezer with api search requests.
-	sleep .25s
-done
+DownloadURL(){
+	logit "Starting Download ... "
+	DLURL=${1}
+	./SMLoadr-linux-x64 -q ${quality} -p "${downloadDir}" "${DLURL}" 
+	logit "Download Complete"
+}
 
-#Take all entries from wantedArtistID.txt remove anything that isn't a number, save into temp file, move temp file back to original.
-sed 's/[^0-9]*//g' $scriptDir/wantedArtistID.txt > $scriptDir/wantedArtistID-temp.txt && mv $scriptDir/wantedArtistID-temp.txt $scriptDir/wantedArtistID.txt
 
-#Take all entries from wantedArtistID.txt remove any duplicates, save into temp file, move temp file back to original.
-awk '!a[$0]++' $scriptDir/wantedArtistID.txt > $scriptDir/wantedArtistID-temp.txt && mv $scriptDir/wantedArtistID-temp.txt $scriptDir/wantedArtistID.txt
 
-#Load IDs from wantedArtistID.txt file and Loop through each ID from smloadrartists.
-smloadrArtists=$(cat "$scriptDir/wantedArtistID.txt")
-for smloadrArtist in $smloadrArtists;
-	do
-	#Download smloadrArtist with smloadr to downloadDir.
-	echo "Downloading Deezer ID - $smloadrArtist with SMLoadr now."
-	./SMLoadr-linux-x64 -q $quality -p $downloadDir https://www.deezer.com/artist/$smloadrArtist
-done
+ErrorExit(){
+	case ${2} in
+		2)	echo ${1};exit ${2};;
+		144)	echo ${1};exit ${2};;
+		*)	echo ${1} |tee -a ${scriptDir}/${logname};exit ${2};;
+	esac
+}
 
-echo "Task Complete"
+logit(){
+	echo ${1} | tee -a ${scriptDir}/${logname}
+}
+
+skiplog(){
+	echo ${1} | tee -a ${scriptDir}/${skiplogname}
+}
+
+InitLogs(){
+	echo "Beginning Log" |tee ${scriptDir}/${logname} || ErrorExit "Cant create log file" 144
+	echo "LidArtistName;DeezerArtistID;DeezerArtistURL;LidAlbumName;DeezerDiscog" |tee ${scriptDir}/${skiplogname} || ErrorExit "Cant create skiplog file" 144
+}
+
+WantedModeBegin(){
+	AlbumsLidarrReq
+	GetTotalAlbumsLidarrReq
+	let loopindex=TotalLidAlbumsNames-1
+	logit "Going to process and download ${TotalLidAlbumsNames} records"
+	for ((i=0;i<=(loopindex);i++)); do
+			logit ""
+			LidArtistName=""
+			LidArtistDLName=""
+			DeezerArtistID=""
+			DeezerArtistURL=""
+			LidAlbumName=""
+			DeezerDiscogAlbumName=""
+			DeezerAlbumURL=""
+			DeezerAlbumID=""
+		echo "-Processing"
+		if [ -n "${wantit}" ]; then
+			ProcessAlbumsLidarrReq
+			logit "ArtistName: ${LidArtistName}"
+			logit "LidarrAlbumName: ${LidAlbumName}"
+			logit "ArtistID: ${DeezerArtistID}"
+		else
+			ErrorExit "Lidarr communication error, check lidarrUrl in config or lidarrApiKey"
+		fi
+		echo "-Querying"
+		if [ -n "${DeezerArtistID}" ] || [ -n "${LidArtistName}" ] || [ -n "${LidAlbumName}" ]; then
+			QueryAlbumURL
+			logit "DeezerAlbumName: ${DeezerDiscogAlbumName}"
+			logit "DeezerAlbumURL: ${DeezerAlbumURL}"
+		else
+			logit "Cant get artistname or artistid or albumname .. skipping" 
+			skiplog "${LidArtistName};${DeezerArtistID};${DeezerArtistURL};${LidAlbumName}"
+			continue
+		fi
+		if [ -n "${DeezerAlbumURL}" ]; then
+			DownloadURL "${DeezerAlbumURL}"
+		else
+			logit "Cant match the wanted album to an album on deezer .. skipping" 
+			skiplog "${LidArtistName};${DeezerArtistID};${DeezerArtistURL};${LidAlbumName};${DeezerDiscogArr[*]}"
+			continue
+		fi
+		if [ "${EnableLidarrProcess}" = True ] && [ -n "${LidArtistDLName}" ];then
+				logit "Sending to Lidarr for post Processing"
+				dlloc="${downloadDir}/${LidArtistDLName}/"
+				LidarrProcessIt=$(curl -s "$lidarrUrl/api/v1/command" --header "X-Api-Key:"${lidarrApiKey} --data '{"name":"DownloadedAlbumsScan", "path":"'"$dlloc"'"}' );
+		else
+			logit "Skipping Lidarr Processing"
+		fi
+	done
+}
+
+ArtistModeBegin(){
+	ArtistsLidarrReq
+	GetTotalArtistsLidarrReq
+	let loopindex=TotalLidArtistNames-1
+	logit "Going to process and download ${TotalLidArtistNames} records"
+	for ((i=0;i<=(loopindex);i++)); do
+		logit ""
+		LidArtistName=""
+		DeezerArtistID=""
+		DeezerArtistURL=""
+		LidAlbumName=""
+		DeezerDiscogAlbumName=""
+		DeezerAlbumURL=""
+		echo "-Processing"
+		if [ -n "${wantit}" ]; then
+			ProcessArtistsLidarrReq
+			logit "ArtistName: ${LidArtistName}"
+			logit "LidarrAlbumName: ${LidAlbumName}"
+			logit "ArtistID: ${DeezerArtistID}"
+		else
+			ErrorExit "Lidarr communication error, check lidarrUrl in config or lidarrApiKey"
+		fi
+		echo "-Querying"
+		if [ -n "${DeezerArtistID}" ] || [ -n "${LidArtistName}" ] || [ -n "${DeezerArtistURL}" ]; then
+			DownloadURL "${DeezerArtistURL}"
+			logit "DeezerArtistURL: ${DeezerArtistURL}"
+		else
+			logit "Cant get artistname or or DeezerArtistURL or artistid.. skipping" 
+			skiplog "${LidArtistName};${DeezerArtistID};${DeezerArtistURL};${LidAlbumName}"
+			continue
+		fi
+	done
+}
+
+main(){
+	echo "Starting up"
+	source ./config || ErrorExit "Configuration file not found" 2
+	InitLogs
+	case "${mode}" in 
+		wanted)	WantedModeBegin;;
+		artist) ArtistModeBegin;;
+		*) logit "mode error, check mode variable in config valid = wanted/artist" ;;
+	esac
+}
+
+main ${@}
